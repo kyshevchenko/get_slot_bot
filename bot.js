64b9bf50280c777737f80fb56656683469eadc4f
kiril.shevchenko <kiril.shevchenko@x5.ru>
2024-11-25 23:37:16 +0300

    logic change

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const users = {}; // Список пользователей
+const users = {};
 const iceRincLink = "https://katok.sport.moscow/#registration";
 const reqiredDay = "01.12.2024";
 const reqiredTime = "20:30";
@@ -27,6 +27,22 @@ bot.start((ctx) => {
       `Ты уже был добавлен в список рассылки ранее. Сиди и жди чуда, малыш ✨`
     );
   }
+
+  if (userId == process.env.OWNER_ID) {
+    const usersList = Object.values(users).map(e => `@${e.tag}`);
+    ctx.reply(`Список ждунов: ${usersList}`);
+  }
+});
+
+bot.command("stop", (ctx) => {
+  const userId = ctx.message.from.id;
+  if (users[userId]) {
+    delete users[userId];
+
+    ctx.reply(`Ты удален из списка рассылки.`);
+  } else {
+    ctx.reply(`Тебя уже/еще нет в списке рассылки.`);
+  }
 });
 
 // Функция для проверки доступности слотов
@@ -38,7 +54,7 @@ const waitAvailableSlot = async () => {
 
   const pages = await browser.pages();
   const page = pages[0];
-  await page.setViewport({ width: 1920, height: 1080 });
+  // await page.setViewport({ width: 1920, height: 1080 });
   await page.goto(iceRincLink, { waitUntil: "domcontentloaded" });
 
   console.log("Запустили проверку доступности слотов...");
@@ -51,43 +67,54 @@ const waitAvailableSlot = async () => {
       if (!calendar) return false;
 
       const days = Array.from(calendar.querySelectorAll(".swiper-slide"));
+
       for (const day of days) {
         const dateElement = day.querySelector(".registration-block__subtitle");
-        if (dateElement && dateElement.textContent.trim() === reqiredDay) {
-          return true;
+        if (dateElement && dateElement.textContent === reqiredDay) {
+          const slots = Array.from(
+            day.querySelectorAll(".registration-block__time")
+          );
+          if (slots[slots.length - 1].textContent !== "Мест нет") {
+            return true;
+          }
         }
       }
       return false;
     }, reqiredDay);
 
     if (isDayFound) {
-      console.log("Слот найден!");
+      // console.log("Слот найден!");
 
       const usersArr = Object.keys(users); // Список всех пользователей
       for (const userId of usersArr) {
         try {
           await bot.telegram.sendMessage(
             userId,
-            `Запись на ${reqiredDay} в ${reqiredTime} появилась!\n
-            Поспеши, это уведомление получили еще ${usersArr.length} человек`
+            `Запись на ${reqiredDay} в ${reqiredTime} появилась!\n\n
+Поспеши, это уведомление получили еще несколько человек, количество охотников за слотами: ${usersArr.length}\n
+${iceRincLink}`
           );
         } catch (error) {
-          console.error(`Ошибка при отправке сообщения пользователю ${userId}:`, error.message);
+          console.error(
+            `Ошибка при отправке сообщения пользователю ${userId}:`,
+            error.message
+          );
         }
       }
 
-      clearInterval(intervalId); // Останавливаем проверку
-      await browser.close(); // Закрываем браузер
-    } else {
-      const fullDate = new Date();
-      const date = fullDate.toLocaleDateString();
-      const time = fullDate.toLocaleTimeString();
-      console.log(date, time, "Дата не найдена, продолжаем искать...");
+      // clearInterval(intervalId);
+      // await browser.close();
     }
-  }, 5000);
+    // else {
+    //   const fullDate = new Date();
+    //   const date = fullDate.toLocaleDateString();
+    //   const time = fullDate.toLocaleTimeString();
+    //   console.log(date, time, "Дата не найдена, продолжаем искать...");
+    // }
+  }, 15000);
 };
 
-// Запускаем проверку слотов один раз
+// Запускаем проверку слотов
 waitAvailableSlot();
 
 bot.launch();
